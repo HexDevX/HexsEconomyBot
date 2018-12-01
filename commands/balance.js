597b6a5e118f4f73010fcc557d649a089784849e
outlaylol <43355709+outlaylol@users.noreply.github.com>
2018-12-01 14:05:10 -0600

    updated balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,14 +1,13 @@
-const Discord = require('discord.js')
 const db = require('quick.db')
+const Discord = require('discord.js')
 
 exports.run = async (client, message, args, config) => {
 
     let user = message.mentions.members.first() || message.author;
 
     let money = await db.fetch(`money_${user.id}`)
-    if (money === null) money = 0; // `0` can be whatever.
+    if (money === null) money = 0;
 
-    message.channel.send(user + ' has `' + money + '` money!')
-
+    message.channel.send(`${user} you have ${money}$ !`)
 
 }
